7de475cba2dc8426834840c27cb8a0c3cffee608
David Recinos <drecinos@solutionsda.com>
2022-10-19 13:46:45 -0600

    added EJS

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require('express');
 
 const app = express();
 const port = process.env.PORT || 4000;
-
+app.set('view engine', 'ejs')
 
 app.get(
     "/", (req, res) =>{
-        res.send('Pagina inicio')
+        res.render('pages/index')
     }
 )
 
